c07e0a910bf6e518af42889a22a7ee702d3cf7ad
Nam Nguyen <namnguyen@cpp.edu>
2021-09-27 11:26:34 -0700

    preliminary implementation the classicalMultiplication function

--- ClassicalMultiplication.java
+++ ClassicalMultiplication.java
@@ -1,3 +1,22 @@
 public class ClassicalMultiplication {
-    
+    public int[][] classicalMultiplication(int[][] matrix1, int [][]matrix2) {
+        
+        if(matrix1[0].length != matrix2.length) {
+            return null;
+        }
+        else {
+            int[][] matrix = new int[matrix1.length][matrix2[0].length];
+
+            for(int row = 0; row < matrix1[0].length; row++) {
+                for(int col = 0; col < matrix2.length; col++) {
+
+                }
+            }
+
+
+
+            return matrix;
+        }
+
+    }
 }
